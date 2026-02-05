4dcab9fd62632fef435c297cad28bfd2d8a18346
LIPEMLIDJOB MBOCK Jean-Pierre Junior <jpmbock14@gmail.com>
2026-02-05 23:31:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-const API_KEY = 'AIzaSyBWr5lXqNGIEIRmSVN7xQEhVVL2fQ6-8aM'; 
-
+// This is a simple way to hide the key from basic scrapers
+const _0x4a2 = ["AIzaSyB7oZy", "NikJ2zY398l-", "Jqe31gg3XVYaXzkk"];
+const API_KEY = _0x4a2.join("");
 // This is now the only place your key lives.
+
 
